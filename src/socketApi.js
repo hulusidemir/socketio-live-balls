cb8bf6be631f5288d557b845b06a71a7eff8685e
hulusidemir <hulusidemir89@yahoo.com>
2018-09-14 06:26:20 +0300

    Index Controller

---
+++ src/socketApi.js
@@ -0,0 +1,13 @@
+const socketIo = require('socket.io');
+const io = socketIo();
+
+const socketApi =  {
+
+};
+socketApi.io = io;
+io.on('connection',(socket)=> {
+    console.log('a user connected');
+});
+
+
+module.exports = socketApi;
